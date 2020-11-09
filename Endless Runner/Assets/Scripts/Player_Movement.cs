b0f21df5cc6293cdc2c87d7a13526ba7e1cff97c
Tallak <tallak.lvdl@gmail.com>
2020-11-09 18:39:29 +0100

    Player movement horisontal
    I also added the player with rigidbocy and collider

--- Endless Runner/Assets/Scripts/Player_Movement.cs
+++ Endless Runner/Assets/Scripts/Player_Movement.cs
@@ -6,26 +6,39 @@ public class Player_Movement : MonoBehaviour
 {
     public float jumpForce;
     public float gravity;
+    public float speed = 1;
     
     public Rigidbody2D rb2D;
 
-    private bool isGrounded;
+    public bool isGrounded;
 
     void Start()
     {
         rb2D = transform.GetComponent <Rigidbody2D>();
-        jumpForce = 25f;
-        gravity = 5f;
+        //don't have to do this because it can easily be set and changed in the inspector
+        //jumpForce = 25f;
+        //gravity = 5f;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        
         if (Input.GetKey(KeyCode.Space) && isGrounded)
         {
-            rb2D.AddForce(Vector3.up * jumpForce * 20f);
+
+            rb2D.AddForce(new Vector2(0, 1) * jumpForce * 20f, ForceMode2D.Force);
+
         }
         rb2D.gravityScale = gravity;
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            MapCreator.instance.Move(-speed * Time.deltaTime);
+        } else if (Input.GetKey(KeyCode.A))
+        {
+            MapCreator.instance.Move(speed * Time.deltaTime);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
