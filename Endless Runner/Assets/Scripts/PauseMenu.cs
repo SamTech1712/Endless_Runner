7d3c5c4a40282a10f42c4d3dd32e23274ebe9aab
Tallak <tallak.lvdl@gmail.com>
2020-11-16 19:42:24 +0100

    UI - Main menu and Pause menu
    The image and colors will be swapped for own art

---
+++ Endless Runner/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused;
+
+    public GameObject pauseMenuUI;
+
+    public void PauseGame()
+    {
+        if (!gameIsPaused)
+        {
+            pauseMenuUI.SetActive(true);
+            Time.timeScale = 0f;
+            gameIsPaused = true;
+        }
+        else
+        {
+            ResumeGame();
+        }
+    }
+
+    public void ResumeGame()
+    {
+        if (gameIsPaused)
+        {
+            pauseMenuUI.SetActive(false);
+            Time.timeScale = 1f;
+            gameIsPaused = false;
+        }
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MenuScene");
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Endless Runner");
+    }
+}
