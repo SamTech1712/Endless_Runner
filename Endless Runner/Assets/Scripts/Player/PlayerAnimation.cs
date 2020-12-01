4c37d7d2558a7b379353437da3dc47d583241dff
Tallak <tallak.lvdl@gmail.com>
2020-12-01 18:30:47 +0100

    Animation

---
+++ Endless Runner/Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    public NewPlayerMovement player;
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("Velocity", player.velocity);
+        animator.SetBool("IsGrounded", player.isGrounded);
+    }
+}
